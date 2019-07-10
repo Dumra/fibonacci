0f6264cf96f0293a4a93a7d93661d429a66ef9df
dumra <qh8lh9bl>
2019-07-10 23:54:02 +0300

    Refactoring and add package to calculate exec time

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
+use chrono::Local;
 use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     let mut input_number = String::new();
+    println!("Input ur number:");
     io::stdin()
         .read_line(&mut input_number)
         .expect("Can't read from command line");
 
-    let count_fibonacci: u32 = match input_number.trim().parse() {
+    let count_fibonacci: u64 = match input_number.trim().parse() {
         Ok(number) => {
-            let value: u32 = match (number as u32).cmp(&0) {
+            let value = match (number as u32).cmp(&0) {
                 Ordering::Greater => number,
-                Ordering::Equal => 1,
-                Ordering::Less => 1,
+                Ordering::Equal | Ordering::Less => 1,
             };
             value
         }
@@ -22,17 +23,26 @@ fn main() {
         }
     };
 
+    let start = Local::now().timestamp();
+
+    let last_loop = count_fibonacci - 1;
     for number in 0..count_fibonacci {
-        print!("{}, ", calculate_fibonacci(number));
+        if number == last_loop {
+            print!("{}", calculate_fibonacci(number));
+        } else {
+            print!("{}, ", calculate_fibonacci(number));
+        }
     }
+    print!("\n");
+
+    let end = Local::now().timestamp();
+    println!("{} seconds for whatever you did.", end - start);
 }
 
-fn calculate_fibonacci(number: u32) -> u32 {
-    if number == 0 {
-        0
-    } else if number == 1 {
-        1
-    } else {
-        calculate_fibonacci(number - 1) + calculate_fibonacci(number - 2)
+fn calculate_fibonacci(number: u64) -> u64 {
+    match number {
+        0 => 0,
+        1 => 1,
+        number => calculate_fibonacci(number - 1) + calculate_fibonacci(number - 2),
     }
 }
